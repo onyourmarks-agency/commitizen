0416b4a4681affb10e5cc4718833a7b2817f4972
Jordi Kroon <jkroon@onyourmarks.agency>
2024-11-08 10:59:58 +0100

    copy(config): update config

--- internal/config/default.go
+++ internal/config/default.go
@@ -9,29 +9,29 @@ items:
    group: page1
    options:
      - value: feat
-       key: "Feat:\t🎉\tA new feature"
+       key: "Feat:  🎉\tA new feature"
      - value: fix
-       key: "Fix:\t🪲\tA bug fix"
+       key: "Fix:   🪲\tA bug fix"
      - value: merge
-       key: "Merge:\t🙏\tResolving merge conflicts that were not auto-resolved"
+       key: "Merge: 🙏\tResolving merge conflicts that were not auto-resolved"
      - value: copy
-       key: "Copy:\t✍️\t Textual changes (e.g. HTML, translations, etc)"
+       key: "Copy:  ✍️\t Textual changes (e.g. HTML, translations, etc)"
      - value: style
-       key: "Style:\t✨\tChanges that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)"
+       key: "Style: ✨\tChanges that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)"
      - value: build
-       key: "Build:\t🏗\t Changes that affect how code is built (composer, pnpm, deploy, config-only changes, etc)"
+       key: "Build: 🏗\t Changes that affect how code is built (composer, pnpm, deploy, config-only changes, etc)"
      - value: docs
-       key: "Docs:\t📚\tDocumentation only changes"
+       key: "Docs:  📚\tDocumentation only changes"
  - name: scope
    label: "Scope. Could be anything specifying place of the commit change (user, db, poll):"
    type: string
-   group: page2
+   group: page1
    trim: true
  - name: subject
    label: "Subject. Concise description of the changes. Imperative, lower case and no final dot:"
    trim: true
    type: string
-   group: page3
+   group: page1
    required: true
 
 format: "{{.type}}{{with .scope}}({{.}}){{end}}: {{.subject}}"`
