b2fdfca671c81988534f0ffd14b0d3d130ee60ae
Jordi Kroon <jkroon@onyourmarks.agency>
2024-11-08 10:51:10 +0100

    feat(copy): use oym repo information

--- internal/config/default.go
+++ internal/config/default.go
@@ -4,29 +4,34 @@ const DefaultCommitTemplate = `name: oym-cz
 default: true
 items:
  - name: type
-   desc: "Select the type of change that you're committing:"
-   type: list
+   label: "Select the type of change that you're committing:"
+   type: select
+   group: page1
    options:
-     - name: feat
-       desc: "🎉\tA new feature"
-     - name: fix
-       desc: "🪲\tA bug fix"
-     - name: merge
-       desc: "🙏\tResolving merge conflicts that were not auto-resolved"
-     - name: copy
-       desc: "✍️\t Textual changes (e.g. HTML, translations, etc)"
-     - name: style
-       desc: "✨\tChanges that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)"
-     - name: build
-       desc: "🏗\t Changes that affect how code is built (composer, pnpm, deploy, config-only changes, etc)"
-     - name: docs
-       desc: "📚\tDocumentation only changes"
+     - value: feat
+       key: "🎉\tA new feature"
+     - value: fix
+       key: "🪲\tA bug fix"
+     - value: merge
+       key: "🙏\tResolving merge conflicts that were not auto-resolved"
+     - value: copy
+       key: "✍️\t Textual changes (e.g. HTML, translations, etc)"
+     - value: style
+       key: "✨\tChanges that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)"
+     - value: build
+       key: "🏗\t Changes that affect how code is built (composer, pnpm, deploy, config-only changes, etc)"
+     - value: docs
+       key: "📚\tDocumentation only changes"
  - name: scope
-   desc: "Scope. Could be anything specifying place of the commit change (user, db, poll):"
+   label: "Scope. Could be anything specifying place of the commit change (user, db, poll):"
    type: input
+   group: page2
+   trim: true
  - name: subject
-   desc: "Subject. Concise description of the changes. Imperative, lower case and no final dot:"
+   label: "Subject. Concise description of the changes. Imperative, lower case and no final dot:"
+   trim: true
    type: input
+   group: page3
    required: true
 
 format: "{{.type}}{{with .scope}}({{.}}){{end}}: {{.subject}}"`
